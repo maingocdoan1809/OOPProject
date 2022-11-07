9aaa45c8af7ebce85b464be910f5041f786f407c
Doandeptrai1809 <maingocdoan.work@gmail.com>
2022-11-07 22:47:23 +0700

    new graphView GUI

--- Dijkstra/src/main/java/huce/View/GraphView.java
+++ Dijkstra/src/main/java/huce/View/GraphView.java
@@ -2,10 +2,7 @@ package huce.View;
 
 import com.mindfusion.diagramming.Shape;
 import com.mindfusion.diagramming.*;
-import com.mindfusion.drawing.Align;
-import com.mindfusion.drawing.Brush;
-import com.mindfusion.drawing.Pen;
-import com.mindfusion.drawing.Pens;
+import com.mindfusion.drawing.*;
 import huce.Algorithm.Node.Node;
 
 import javax.swing.*;
@@ -19,6 +16,11 @@ public class GraphView extends JFrame {
     TreeMap<String, DiagramNode> diagramNodes;
     TreeMap<String, Node> nodes;
     Diagram diagram;
+    private javax.swing.JButton jBtnAnelLayout;
+    private javax.swing.JButton jBtnCirLayout;
+    private javax.swing.JButton jBtnReload;
+    private javax.swing.JButton jBtnSpringLayout;
+    private javax.swing.JButton jBtnStart;
     static private TreeMap<String, DiagramNode> toDiagramNodes(Set<String> nodes,
                                                                Diagram diagram) {
         TreeMap<String, DiagramNode> diagramNodes = new TreeMap<>();
@@ -61,63 +63,130 @@ public class GraphView extends JFrame {
         diagram.setShowHandlesOnDrag(true);
         diagram.setAllowLinksRepeat(false);
     }
-    public void drawPath(TreeSet<Node> path, Pen colorPen, Brush colorBrush, int layer) {
-        var pathArr = path.toArray();
-        for ( int index = pathArr.length - 1; index >= 1; index -- ) {
-            String currNode = ((Node) pathArr[index]).getName();
-            String preNode =  ((Node) pathArr[index - 1]).getName();
-            var links = diagramNodes.get(currNode).getIncomingLinks();
-            for ( DiagramLink link : links ) {
-                if ( link.getOrigin() == diagramNodes.get(preNode)) {
-                    if ( link.getPen() != null ) {
-                        link.setPen(Pens.YellowGreen);
-                        link.setHeadPen(Pens.YellowGreen);
-                    } else {
-                        link.setPen(colorPen);
-                        link.setHeadPen(colorPen);
+    synchronized public void drawPath(TreeSet<Node> path, Pen colorPen, Brush colorBrush,
+                                      int layer) {
+        Thread thread = new Thread(() -> {
+            var pathArr = path.toArray();
+            for ( int index = 0; index < pathArr.length - 1; index ++ ) {
+                try {
+                    Thread.sleep(800);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                String currNode = ((Node) pathArr[index]).getName();
+                String preNode =  ((Node) pathArr[index + 1]).getName();
+                var currDigNode = diagramNodes.get(currNode);
+                diagramNodes.get(preNode).setBrush(Brushes.DarkOrange);
+                var links = currDigNode.getOutgoingLinks();
+                for ( DiagramLink link : links ) {
+                    if ( link.getDestination() == diagramNodes.get(preNode)) {
+                        if ( link.getPen() != null ) {
+                            link.setPen(Pens.YellowGreen);
+                            link.setHeadPen(Pens.YellowGreen);
+                        } else {
+                            link.setPen(colorPen);
+                            link.setHeadPen(colorPen);
+                        }
+                        link.setLayerIndex(layer);
+                        link.setTextBrush(colorBrush);
+                        break;
                     }
-                    link.setLayerIndex(layer);
-                    link.setTextBrush(colorBrush);
-                    break;
                 }
             }
-        }
+        });
+        thread.start();
     }
-    public GraphView(TreeMap<String, Node> nodes) {
-        super("Graphic illustration for Dijkstra Algorithm. Author: Mai Ngoc Doan");
+
+    private void initComponents(TreeMap<String, Node> nodes) {
+
+
+        javax.swing.JPanel jPanel1 = new javax.swing.JPanel();
+
         this.nodes = nodes;
         this.diagram = new Diagram();
-        Rectangle2D.Float bounds = new Rectangle2D.Float(0, 0, 15, 15);
+//        Rectangle2D.Float bounds = new Rectangle2D.Float(0, 0, 15, 15);
         this.diagramNodes = toDiagramNodes(nodes.keySet(), diagram);
-        DiagramView diagramView = new DiagramView(diagram);
-        JScrollPane scrollPane = new JScrollPane(diagramView);
-        CircularLayout layout = new CircularLayout();
-        diagramSetUp(diagram, layout);
-        var groupLayout = new GroupLayout(this.getContentPane());
-        diagramView.setAllowLinkCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        com.mindfusion.diagramming.DiagramView diagramView =
+                new com.mindfusion.diagramming.DiagramView(diagram);
+        javax.swing.JScrollPane jScrollPane1 = new javax.swing.JScrollPane(diagramView);
+        CircularLayout circularLayoutlayout = new CircularLayout();
+        circularLayoutlayout.setRadius(70f);
+        diagramSetUp(diagram, circularLayoutlayout);
+        jBtnReload = new javax.swing.JButton();
+        jBtnStart = new javax.swing.JButton();
+        jBtnAnelLayout = new javax.swing.JButton();
+        jBtnCirLayout = new javax.swing.JButton();
+        jBtnSpringLayout = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jScrollPane1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jScrollPane1.setMaximumSize(new java.awt.Dimension(3000, 3000));
+        jScrollPane1.setName(""); // NOI18N
+        jScrollPane1.setVerifyInputWhenFocusTarget(false);
+        jScrollPane1.setViewportView(diagramView);
+
+        diagramView.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        diagramView.setHorizontalResizeCursor(new java.awt.Cursor(java.awt.Cursor.E_RESIZE_CURSOR));
+        diagramView.setMaximumSize(new java.awt.Dimension(3276, 3276));
+        diagramView.setMinimumSize(new java.awt.Dimension(500, 500));
+        diagramView.setName(""); // NOI18N
+        diagramView.setPreferredSize(new java.awt.Dimension(500, 500));
         diagramView.setBehavior(Behavior.PanAndModify);
-        diagramView.setCounterDiagonalResizeCursor(new java.awt.Cursor(java.awt.Cursor.SW_RESIZE_CURSOR));
-        diagramView.setHorizontalResizeCursor(new java.awt.Cursor(java.awt.Cursor.MOVE_CURSOR));
-        groupLayout.setHorizontalGroup(
-                groupLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(groupLayout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(scrollPane,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, 1000,
-                                        Short.MAX_VALUE)
-                                .addContainerGap())
+        javax.swing.GroupLayout diagramViewLayout = new javax.swing.GroupLayout(diagramView);
+        diagramView.setLayout(diagramViewLayout);
+        diagramViewLayout.setHorizontalGroup(
+                diagramViewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 3780, Short.MAX_VALUE)
         );
-        groupLayout.setVerticalGroup(
-                groupLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(groupLayout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(scrollPane,
-                                        javax.swing.GroupLayout.DEFAULT_SIZE, 700,
-                                        Short.MAX_VALUE)
-                                .addContainerGap())
+        diagramViewLayout.setVerticalGroup(
+                diagramViewLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 3780, Short.MAX_VALUE)
         );
-        this.getContentPane().setLayout(groupLayout);
+
+        jScrollPane1.setViewportView(diagramView);
+
+        jPanel1.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.RIGHT));
+
+        jBtnReload.setText("Reload");
+        jPanel1.add(jBtnReload);
+
+        jBtnStart.setLabel("Start");
+        jPanel1.add(jBtnStart);
+
+        jBtnAnelLayout.setLabel("AnnealLayout");
+        jPanel1.add(jBtnAnelLayout);
+
+        jBtnCirLayout.setText("Circular Layout");
+        jPanel1.add(jBtnCirLayout);
+
+        jBtnSpringLayout.setLabel("Spring Layout");
+        jPanel1.add(jBtnSpringLayout);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 1200,
+                                Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addComponent(jScrollPane1,
+                                        javax.swing.GroupLayout.DEFAULT_SIZE, 750,
+                                        Short.MAX_VALUE)
+                                .addGap(0, 0, 0)
+                                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        this.pack();
+        this.setLocationRelativeTo(null);
+        pack();
+    }// </editor-fold>
+    public GraphView(TreeMap<String, Node> nodes) {
+        super("Graphic illustration for Dijkstra Algorithm. Author: Mai Ngoc Doan");
+        initComponents(nodes);
+
     }
 }
